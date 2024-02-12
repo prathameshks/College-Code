268bc7e97a950eaa789719d5a7d7fffa11663042
Prathamesh Sable <82529994+prathameshks@users.noreply.github.com>
2024-02-12 16:38:13 +0530

    Wtl practical 4 calculator

--- WTL/practical 4/script.js
+++ WTL/practical 4/script.js
@@ -1,23 +1,41 @@
 var i = document.getElementById("input");
 i.value = '';
+var reset = false;
 
-function btnFunction(s){
-    if(s == 'c'){
+function btnFunction(s) {
+    if (reset) {
         i.value = '';
-    }else if(s == '='){
+        i.style.border = "none";
+        reset = false;
+    }
+    if (s == 'c') {
+        i.value = '';
+    } else if (s == '=') {
         try {
             i.value = eval(i.value);
+            if (i.value == 'Infinity' || i.value == '-Infinity' || i.value == 'NaN') {
+                i.style.border = "1px solid red";
+                reset = true;
+                throw 1;
+            } else {
+                reset = false;
+            }
         } catch (error) {
-            i.style.border = "1px solid red"; 
+            i.style.border = "1px solid red";
+            reset = true;
             return;
         }
-    }else if(s== '^'){
-        i.value += '**';
+    } else if (s == 'back') {
+        i.value = i.value.slice(0, -1);
     }
-    i.style.border = "none"; 
+    i.style.border = "none";
 }
 
-function btnClick(s){
-    // console.log(s);
+function btnClick(s) {
+    if (reset) {
+        i.value = '';
+        i.style.border = "none";
+        reset = false;
+    }
     i.value += s;
 }
